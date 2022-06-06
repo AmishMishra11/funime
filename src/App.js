149ad12c9024a025e222fcedee9d511e0f2616c2
AmishMishra11 <amishmishra11@gmail.com>
2022-06-06 22:45:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App  text-lg border-2 rounded-lg border-red-500  m-3">
+      Development in process...
+      <div className="bg-primaryLight p-3 ">Colors</div>
+      <div className="bg-primaryDark p-3 ">Colors</div>
+      <div className="bg-secondaryLight p-3 ">Colors</div>
+      <div className="bg-secondaryDark p-3 ">Colors</div>
+      <div className="bg-darkLight text-secondaryLight p-3 ">Colors</div>
+      <div className="bg-darkDark text-secondaryDark p-3 ">Colors</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryLight: "#D8B4FE",
+        primaryDark: "#9333EA",
+        secondaryLight: "#FFFFFF",
+        secondaryDark: "#F8FAFC",
+        darkLight: "#1E293B",
+        darkDark: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+};
